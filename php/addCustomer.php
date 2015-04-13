6de847d7f1d434bb16d410edc8017bfcea1f122e
RadRadRobotank91 <hwf2@pitt.edu>
2015-04-13 10:38:16 -0400

    Updates to addCustomer.php
    Made some updates to addCustomer.php based on changes made to main.php.

--- php/addCustomer.php
+++ php/addCustomer.php
@@ -1,40 +1,14 @@
-IDs (do they need to be names?)
-membership - CSA type
-coffeeSelection - coffee type
-beans - coffee bean type
-standardLocations - standard pick-up locations
-marketLocations - market pick-up locations
-otherMembers - other people allowed to pick up
-firstName - first name
-lastName - last name
-address - address
-city - city
-zip - zip
-phoneNumber - phone number
-email - email
-email2 - verify email (needed?)
-marketing - marketing
-marketingOther - marketing other
-workshare - workshare
-check - check
-acknowledge - acknowledge (needed?)
-
  <?php
-/*
-* Get values from POST
-* Some of these are IDs, not names...not sure if that will make a difference or not
-* Need to consolidate membership, coffeeSelection, standardLocations, and marketLocations from 4 to 2 variables for proper database entry
-*/
-$membership = $_POST['membership'];//csaid (1-4)
-$coffeeSelection = $_POST['coffeeSelection'];//csaid (5-8)
-$beans = $_POST['beans'];//csaid (5-8)
-$standardLocations = $_POST['standardLocations'];//lid
-$marketLocations = $_POST['marketLocations'];//lid
+//Get values from POST
+$membership = $_POST['cat'];//csaid (1-4)
+$coffeeSelection = $_POST['cat2'];//csaid (5-8)
+$locations = $_POST['subcat'];//lid
 $otherMembers = $_POST['otherMembers'];//delegates
 $fName = $_POST['firstName'];//first_name
 $lName = $_POST['lastName'];//last_name
 $address = $_POST['address'];//address
 $city = $_POST['city'];//city
+$state = $_POST['state'];//state
 $zip = $_POST['zip'];//zip
 $phone = $_POST['phoneNumber'];//phone
 $email = $_POST['email'];//email
@@ -42,10 +16,10 @@ $email = $_POST['email'];//email
 $marketing = $_POST['marketing'];//mid
 $marketingOther = $_POST['marketingOther'];//midOther
 //Workshare and Check may require an email sent to OWF and customer, respectively.
-$workshare = $_POST['workshare'];//May not be required, not seen in DB
-$check = $_POST['check'];//balance (if marketshare)
-//Did not add acknowledge, as not sure if needed
+$workshare = $_POST['workshare'];//workshare
+$check = $_POST['check'];//Not present in database
 
+/*
 //Connect to the database
 $link = mysql_connect('holliday.startlogicmysql.com', 'db2560', 'OWF3r$$'); 
 if (!$link) { 
@@ -55,27 +29,42 @@ echo 'Connected successfully';
 
 //Set active database
 mysql_select_db(owf);
-
-
+*/
+//Connect to the database
+require "config.php";
+mysql_select_db(owf);
 
 /*
  * Place customer information into Customer table
  * Format example: 'Harrison', 'Smith', '123 Main Street', 'Pittsburgh', 'PA', '90210', '867-5309', 'test@test.net', 'Kurt, Maggie', 3, 'Margaret', 0
  */
-$query1 = mysql_query("INSERT INTO Customer (First_Name, Last_Name, Address, City, State, Zip, Phone, Email, Delegates, mid, midOther, workshare) VALUES ('" .$fName. "','" .$lName. "','" .$address. "','" .$city. "','" .$zip. "','" .$phone. "','" .$email. "','" .$otherMembers. "'," .$marketing. ",'" .$marketingOther. "'" .$workshare. ")");
-if(!$query1) {
-	die('Invalid query 1: ' . mysql_error());
-}
+mysql_query("INSERT INTO Customer (First_Name, Last_Name, Address, City, State, Zip, Phone, Email, Delegates, mid, midOther, workshare) VALUES ('" .$fName. "','" .$lName. "','" .$address. "','" .$city. "','" .$zip. "','" .$phone. "','" .$email. "','" .$otherMembers. "'," .$marketing. ",'" .$marketingOther. "'" .$workshare. ")");
+
 //Get the ID generated in last query, which is the customer ID
 $customerID = mysql_insert_id;
 
 /*
 * Place customerID, CSA type, and location into Cust_has_CSA table
+* Multiple entries needed if Customer has more than one CSA
 * Format example: 1, 3, 5
 */
-$query2 = mysql_query("INSERT INTO Cust_has_CSA (cid, csaid, lid) VALUES (" .$customerID. "," .$membership. "," .$standardLocations. ")");
-if(!$query2) {
-	die('Invalid query 2: ' . mysql_error());
+
+if($membership == 99) {
+	//No CSA present, coffee only
+	//Insert coffeeSelection into table
+	mysql_query("INSERT INTO Cust_has_CSA (cid, csaid, lid) VALUES (" .$customerID. "," .$coffeeSelection. "," .$locations. ")");
+} else if($membership != 99) {
+	//CSA is present, check for coffee
+	if($coffeeSelection == 99) {
+		//No coffee
+		//Insert membership into table
+		mysql_query("INSERT INTO Cust_has_CSA (cid, csaid, lid) VALUES (" .$customerID. "," .$membership. "," .$locations. ")");
+	} else if($coffeeSelection != 99) {
+		//Coffee
+		//Insert both membership and coffeeSelection into table
+		mysql_query("INSERT INTO Cust_has_CSA (cid, csaid, lid) VALUES (" .$customerID. "," .$membership. "," .$locations. ")");
+		mysql_query("INSERT INTO Cust_has_CSA (cid, csaid, lid) VALUES (" .$customerID. "," .$coffeeSelection. "," .$locations. ")");
+	}
 }
 /*
 * Determine if CSA is Market share
@@ -84,11 +73,6 @@ if(!$query2) {
 if($membership == 3 || $membership == 4) {
 	//Get initial balance from CSAType table
 	$balance = mysql_query("SELECT price FROM CSAType WHERE csaid=" .$membership);
-	$query3 = mysql_query("INSERT INTO Cust_has_Bal (cid, balance) VALUES (" .$customerID. "," .$balance. ")");
-	if(!$query3) {
-	die('Invalid query 3: ' . mysql_error());
+	mysql_query("INSERT INTO Cust_has_Bal (cid, balance) VALUES (" .$customerID. "," .$balance. ")");
 }
-}
-
-mysql_close($link);
 ?>
