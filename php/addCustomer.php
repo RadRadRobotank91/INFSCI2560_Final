322b1e1f939064abb2309ba9dd3c471b254c1a47
relaxabyte <Kurt@Mauras-iMac.home>
2015-04-19 00:13:40 -0400

    Front End Form Completed
    AddCustomer changed to include a thank you page. I created a thank you
    page that gets inserted into add customer.php

--- php/addCustomer.php
+++ php/addCustomer.php
@@ -1,14 +1,3 @@
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<!--meta http-equiv="refresh" content="5; url=../main.php" /-->
-	
-	<title>Customer Success</title>
-	
-</head>
-<body>
  <?php
 //Get values from POST
 $membership = $_POST['cat'];//csaid (1-4)
@@ -23,10 +12,9 @@ $state = $_POST['state'];//state
 $zip = $_POST['zip'];//zip
 $phone = $_POST['phoneNumber'];//phone
 $email = $_POST['email'];//email
-//Did not add email2 (verification), as not sure if needed
 $marketing = $_POST['marketing'];//mid
 $marketingOther = $_POST['marketingOther'];//midOther
-//Workshare and Check may require an email sent to OWF and customer, respectively.
+//Workshare and Check may require an email sent to OWF and customer, respectively. Future project addition
 $workshare = $_POST['workshare'];//workshare (0-1)
 $payment = $_POST['check'];//payment (0-1)
 $acknowledge = $_POST['acknowledge'];//acknowle (0-1)
@@ -54,66 +42,22 @@ $query = $dbo->prepare("INSERT INTO Customer(First_Name, Last_Name, Address, Cit
 if($query->execute(array('First_Name'=>$fName, 'Last_Name'=>$lName, 'Address'=>$address, 'City'=>$city, 'State'=>$state, 'Zip'=>$zip, 'Phone'=>$phone, 'Email'=>$email, 'Delegates'=>$otherMembers, 'mid'=>$marketing, 'midOther'=>$marketingOther, 'workshare'=>$workshare, 'pay_id'=>$payment, 'aid'=>$acknowledge))) {
 	$customerID = $dbo->lastInsertId();
 	$querySuccess = true;
-	$result = $dbo->prepare("SELECT * FROM Customer");
-	$result->execute();
-	echo "<table>";
-	while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-		echo "<tr>";
-		foreach($row as $value) {
-			echo "<td>{$value}</td>";
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
+	// $result = $dbo->prepare("SELECT * FROM Customer");
+	// $result->execute();
+	// echo "<table>";
+	// while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+	// 	echo "<tr>";
+	// 	foreach($row as $value) {
+	// 		echo "<td>{$value}</td>";
+	// 	}
+	// 	echo "</tr>";
+	// }
+	// echo "</table>";
 } else {
 	echo ("<p>1 - Error inserting Customer information into database.  Please contact Site Administrator.</p>");
 }
 
- echo ("customerID: ".$customerID);
-
-/*
-* Place customerID, CSA type, and location into Cust_has_CSA table
-* Multiple entries needed if Customer has more than one CSA
-* Format example: 1, 3, 5
-*/
-// if($membership == 0) {
-// 	//No CSA present, coffee only
-// 	//Insert coffeeSelection into table
-// 	$query = $dbo->prepare("INSERT INTO Cust_has_CSA (cid, csaid, lid) values ('$customerID','$coffeeSelection','$locations')");
-// 	if($query->execute(array('cid'=>$customerID, 'csaid'=>$coffeeSelection, 'lid'=>$locations))) {
-// 		$querySuccess = true;
-// 	} else {
-// 		echo ("<p>2 - Error inserting Customer information into database.  Please contact Site Administrator.</p>");
-// 	}
-
-// } else if($membership != 0) {
-// 	//CSA is present, check for coffee
-// 	if($coffeeSelection == 0) {
-// 		//No coffee
-// 		//Insert membership into table
-// 		$query = $dbo->prepare("INSERT INTO Cust_has_CSA (cid, csaid, lid) values ('$customerID','$membership','$locations')");
-// 		if($query->execute(array('cid'=>$customerID, 'csaid'=>$membership, 'lid'=>$locations))) {
-// 			$querySuccess = true;
-// 		} else {
-// 			echo ("<p>2 - Error inserting Customer information into database.  Please contact Site Administrator.</p>");
-// 		}
-// 	} else if($coffeeSelection != 0) {
-// 		//Coffee
-// 		//Insert both membership and coffeeSelection into table
-// 		$query = $dbo->prepare("INSERT INTO Cust_has_CSA (cid, csaid, lid) values ('$customerID','$membership','$locations')");
-// 		if($query->execute(array('cid'=>$customerID, 'csaid'=>$membership, 'lid'=>$locations))) {
-// 			$querySuccess = true;
-// 		} else {
-// 			echo ("<p>2 - Error inserting Customer information into database.  Please contact Site Administrator.</p>");
-// 		}
-// 		$query = $dbo->prepare("INSERT INTO Cust_has_CSA (cid, csaid, lid) values ('$customerID','$coffeeSelection','$locations')");
-// 		if($query->execute(array('cid'=>$customerID, 'csaid'=>$coffeeSelection, 'lid'=>$locations))) {
-// 			$querySuccess = true;
-// 		} else {
-// 			echo ("<p>2 - Error inserting Customer information into database.  Please contact Site Administrator.</p>");
-// 		}
-// 	}
-// }
+ // echo ("customerID: ".$customerID);
 
 if(!empty($membership) || $membership != 99) {
 	//CSA has Value
@@ -160,22 +104,22 @@ if($membership == 3 || $membership == 4) {
 	}
 }
 
-if($querySuccess) {
-	echo ("<p>Customer information successfully entered into database.</p>");
-	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
-}
-if($querySuccess1) {
-	echo ("<p>Cust Has CSA Membership successfully entered into database.</p>");
-	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
-}
-if($querySuccess2) {
-	echo ("<p>Cust has CSA Coffee  successfully entered into database.</p>");
-	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
-}
-if($querySuccess3) {
-	echo ("<p>Cust has marketshare balance successfully entered into database.</p>");
-	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
-}
+include 'thankyou.php';
+
+// if($querySuccess) {
+// 	echo ("<p>Customer information successfully entered into database.</p>");
+// 	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
+// }
+// if($querySuccess1) {
+// 	echo ("<p>Cust Has CSA Membership successfully entered into database.</p>");
+// 	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
+// }
+// if($querySuccess2) {
+// 	echo ("<p>Cust has CSA Coffee  successfully entered into database.</p>");
+// 	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
+// }
+// if($querySuccess3) {
+// 	echo ("<p>Cust has marketshare balance successfully entered into database.</p>");
+// 	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
+// }
 ?>
-</body>
-</html>
---
+++ php/thankyou.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name='robots' content='index,follow' />
+	<meta name='googlebot' content='index,follow' />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>Sign up Now! | One Woman Farm</title>
+
+	<link rel="alternate" type="application/rss+xml" title="One Woman Farm" href="http://www.onewomanfarm.com/rss.xml" />
+	<link rel="stylesheet" href="../css/screen.css" type="text/css" media="screen, projection">	
+	<link rel="stylesheet" href="../css/print.css" type="text/css" media="print">
+	
+	<!--[if IE]>
+		<link rel="stylesheet" href="/templates/template204/styles/blueprint/blueprint/ie.css" type="text/css" media="screen, projection">
+		<![endif]-->
+
+		<link rel="stylesheet" type="text/css" href="../css/publicglobal.css" media="screen, projection" />
+		<link rel="stylesheet" type="text/css" href="../css/superfish.css" media="screen, projection" />
+		<link rel="stylesheet" type="text/css" href="../css/superfish-vertical.css" media="screen, projection" />
+		<link rel="stylesheet" type="text/css" href="../css/default.css" media="screen, projection" />
+		<link  rel="stylesheet" type="text/css" href="../css/jquery-ui-1.9.2.custom.min.css" media="screen, projection" />
+		<script src="../js/jquery/jquery.js" type="text/javascript"></script>
+		<script src="../js/superfish.js" type="text/javascript"></script>
+		<script src="../js/form.js" type="text/javascript"></script>
+		<link href="../css/content.css" rel="stylesheet" type="text/css" media="screen,tv,projection" />
+		<script type="text/javascript"> 
+			$(document).ready(function(){ 
+				$("ul.sf-menu").superfish(); 
+			}); 
+		</script>
+
+	<!--[if IE 6]>
+		<style type="text/css">
+		@import url(/templates/template204/styles/ie.css);
+		</style> 
+		<![endif]-->
+
+	<!--[if IE 7]>
+		<style type="text/css">
+		@import url(/templates/template204/styles/ie7.css);
+		</style> 
+		<![endif]-->
+
+	<style type='text/css'>
+		/* Local Overrides to CMS Declarations */ 
+		div#imagegrid_image1 {
+			background-image:url("../images/backTruck.jpg");
+			background-position: -5px -16px;
+		}	
+		div#imagegrid_image2 {
+			background-image:url("../images/truckField.jpg");
+			background-position: -18px -96px;
+		}	
+		div#imagegrid_image3 {
+			background-image:url("../images/veggies.jpg");
+			background-position: -87px -20px;
+		}	
+		div#imagegrid_image4 {
+			background-image:url("../images/veggieSale.jpg");
+			background-position: -89px -183px;
+		}	
+		div#imagegrid_image5 {
+			background-image:url("../images/field.jpg");
+			background-position: -46px -122px;
+		}	
+		div#imagegrid_internal #imagegrid_image6 {
+			background-image:url("../images/ground.jpg");
+			background-position: -1px -29px;
+		}	
+		div#imagegrid_internal #imagegrid_image7 {
+			background-image:url("../images/hiddenMargaret.jpg");
+			background-position: -0px -16px;
+		}	
+		div#imagegrid_internal #imagegrid_image8 {
+			background-image:url("../images/openFruit.jpg");
+			background-position: -0px -56px;
+		}	
+		div#logo h1 .name-pt1 {
+			color: #c2beb2;
+		}
+		div#logo h1 .name-pt2 {
+			color: #8b836e;
+		}
+		body {
+			background-color: #c2beb2;
+		}
+		div#imagegrid, div#imagegrid_internal {
+			background-color: #d9d473;
+		}
+		div#content_wrap {
+			background-color: #c2beb2;
+		}
+		ul.main_nav li, div#main_nav, div#footer, ul.main_nav li, ul.main_nav li.active {
+			background-color: #8b836e;
+		}
+		ul.main_nav li.active a:hover,ul.main_nav li.sfHover a:hover {
+			background-color:#c2beb2;
+		}
+		a {
+			color: #8b836e;
+		}			
+		a:hover {
+			background-color: #8b836e;
+		}
+		.frontpage h1, .frontpage h1 a,.frontpage h1 a:hover {
+			color: #8b836e;
+			background-color:transparent;
+		}
+	</style>
+</head>
+<body>
+	<div id="site_wrap" class="">
+		<div class="container">
+			<div id="header">	
+				<div id="header_content">
+					<div id="logo">
+						<span class='imagesprite imagesprite-windmill'></span>
+						<span class='imagesprite imagesprite-barn'></span>						
+						<h1><a href="http://www.onewomanfarm.com">
+							<span class='name-pt1'>One</span><span class='name-pt2'>WomanFarm</span></a>
+						</h1>
+					</div>
+					<div id="contact">
+						<ul>
+							<li><a href="http://www.onewomanfarm.com/contact" id="contact_us">Contact</a></li>
+							<li><a href="http://www.onewomanfarm.com/dynamic_content/xml/rss.xml" id="rss_feed">RSS Feed</a></li>
+						</ul>		
+					</div>
+				</div>
+			</div> <!-- end #header -->
+
+			<div id="imagegrid_internal">
+				<div id="imagegrid_internal_content">
+					<div id="imagegrid_image6" class='imagegrid_image imagegrid_internal_image'> </div>
+					<div id="imagegrid_image7" class='imagegrid_image imagegrid_internal_image'> </div>
+					<div id="imagegrid_image8" class='imagegrid_image imagegrid_internal_image'> </div>
+					<div class='clear_float'></div>
+				</div> <!-- end #imagegrid_internal_content -->
+			</div><!-- end #imagegrid_internal -->
+
+			<div id="content_wrap">
+				<div id="main_nav">
+					<ul  class='main_nav sf-menu'>
+						<li ><a class='toplevel' href='http://www.onewomanfarm.com/'>Home</a>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/description'>One Woman Farm, Inc.</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/description'>One Woman Farm, Inc.</a></li>
+							</ul>
+						</li>
+						<li ><a class='toplevel' href='http://www.onewomanfarm.com'>About Us</a>
+							<ul>
+								<li><a href='/staff'>Staff</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/staff'>Staff</a></li>
+							</ul>
+						</li>
+						<li ><a class='toplevel' href='http://www.onewomanfarm.com'>Get Involved with OWF</a>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/want-to-be-an-owf-csa-distribution-site'>Want to be an OWF CSA distribution site?</a></li>
+								<li><a href='http://www.onewomanfarm.com/apprentice-and-internships-at-owf'>Apprentice and Internships at OWF</a></li>
+								<li><a href='http://www.onewomanfarm.com/csa-workshare-and-volunteer'>CSA Workshare and Volunteer</a></li>
+								<li><a href='http://www.facebook.com/pages/One-Woman-Farm/154385117934782'>Facebook</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/want-to-be-an-owf-csa-distribution-site'>Want to be an OWF CSA distribution site?</a></li>
+								<li><a href='http://www.onewomanfarm.com/apprentice-and-internships-at-owf'>Apprentice and Internships at OWF</a></li>
+								<li><a href='http://www.onewomanfarm.com/csa-workshare-and-volunteer'>CSA Workshare and Volunteer</a></li>
+								<li><a href='http://www.facebook.com/pages/One-Woman-Farm/154385117934782'>Facebook</a></li>
+							</ul>
+						</li>
+						<li class='active'><a class='toplevel' href='http://www.onewomanfarm.com/'>OWF CSA and Farmers Market</a>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-options'>OWF CSA Options!</a></li>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-agreement'>OWF CSA Agreement</a></li>
+								<li><a href='http://www.onewomanfarm.com/one-woman-farm-at-east-liberty-farmers-market'>OWF Farmers Market</a></li>
+								<li><a href='http://www.onewomanfarm.com/pickup-locations'>Pickup Locations</a></li>
+								<li><a href='../index.php'>Sign up Now!</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-options'>OWF CSA Options!</a></li>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-agreement'>OWF CSA Agreement</a></li>
+								<li><a href='http://www.onewomanfarm.com/one-woman-farm-at-east-liberty-farmers-market'>OWF Farmers Market</a></li>
+								<li><a href='http://www.onewomanfarm.com/pickup-locations'>Pickup Locations</a></li>
+								<li><a href='#'>Sign up Now!</a></li>
+							</ul>
+						</li>
+						<li ><a class='toplevel' href='http://www.onewomanfarm.com'>Photos</a>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/gallery'>Photo gallery</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/gallery'>Photo gallery</a></li>
+							</ul>
+						</li>
+						<li ><a class='toplevel' href='http://www.onewomanfarm.com'>Contact OWF</a>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/contact'>Contact us</a></li>
+							</ul>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/contact'>Contact us</a></li>
+							</ul>
+						</li>
+					</ul>				
+					<div class='clear_float'></div>
+				</div> <!--  end #main_nav -->
+
+				<div id="content">
+					<div id="content_container">
+						<div class="col1 span-3">
+							<h2>OWF CSA and Farmers Market</h2>
+							<ul>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-options'>OWF CSA Options!</a></li>
+								<li><a href='http://www.onewomanfarm.com/owf-csa-agreement'>OWF CSA Agreement</a></li>
+								<li><a href='http://www.onewomanfarm.com/one-woman-farm-at-east-liberty-farmers-market'>OWF Farmers Market</a></li>
+								<li><a href='http://www.onewomanfarm.com/pickup-locations'>Pickup Locations</a></li>
+								<li><a href='http://www.onewomanfarm.com/sign-up-now'>Sign up Now!</a></li>
+							</ul>
+						</div>
+						<div class="col2 span-10">
+							<h2>Thank you for registering</h2>
+							<p>Your registration will be fully completed once your check has been received. You will receive a confirmation email confirming that your payment has been received.
+							<p>We are excited to see you this growing season! Welcome to the OWF family.</p>
+						</div>
+					</div>
+					<div id="content_bottom">&nbsp;</div>
+				</div>
+				<div id="footer_wrap">
+					<div id="footer">
+						<div id="footer_content">
+							<div class="col1 span-4">
+								<p><a href="http://www.onewomanfarm.com/contact">Contact Us</a><br />
+									<a href="http://sfc.smallfarmcentral.com">Administrator Login</a></p><br />
+							</div> 
+							<div class="col2 span-7">
+								<p><a href='http://www.onewomanfarm.com/description'>One Woman Farm, Inc.</a> 
+									<a href='http://www.onewomanfarm.com/staff'>Staff</a> 
+									<a href='http://www.onewomanfarm.com/want-to-be-an-owf-csa-distribution-site'>Want to be an OWF CSA distribution site?</a> 
+									<a href='http://www.onewomanfarm.com/apprentice-and-internships-at-owf'>Apprentice and Internships at OWF</a> 
+									<a href='http://www.onewomanfarm.com/csa-workshare-and-volunteer'>CSA Workshare and Volunteer</a> 
+									<a href='http://www.facebook.com/pages/One-Woman-Farm/154385117934782'>Facebook</a> 
+									<a href='http://www.onewomanfarm.com/owf-csa-options'>OWF CSA Options!</a> 
+									<a href='http://www.onewomanfarm.com/owf-csa-agreement'>OWF CSA Agreement</a> 
+									<a href='http://www.onewomanfarm.com/one-woman-farm-at-east-liberty-farmers-market'>OWF Farmers Market</a> 
+									<a href='http://www.onewomanfarm.com/pickup-locations'>Pickup Locations</a> 
+									<a href='#'>Sign up Now!</a> 
+									<a href='http://www.onewomanfarm.com/gallery'>Photo gallery</a> 
+									<a href='http://www.onewomanfarm.com/contact'>Contact us</a></p>			
+							</div>
+							<div class="col3 span-5">
+								<p>All content property of One Woman Farm. 5857 Valencia Rd, Gibsonia, PA 15044.</p>
+								<!-- This page is a fork of the template page created by Small Farm Central -->
+								<p>This page was created using the <a href="http://www.smallfarmcentral.com">Small Farm Central</a> web development service. The web-based user management application was created by the University of Pittsburgh class IS2560.</p>
+							</div>
+							<div class="clear_float">&nbsp;</div>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
