82d944b03e1a5a24ad7852c91a4ac5852e8a4a80
relaxabyte <Kurt@Mauras-iMac.home>
2015-04-20 01:27:15 -0400

    Update Customer Works
    Got it to work.  I didn’t really understand what you were going for
    with locationOne and LocationTwo… etc. I mimicked the logic I used from
    the addCustomer. I jinxed myself by saying in the last update that this
    was almost ready.

--- php/updateCustomer.php
+++ php/updateCustomer.php
@@ -1,97 +1,63 @@
 <?php
+
+
+$customerID = $_POST['customerID'];
+$amount = $_POST['amount'];
+$pickup = $_POST['pickup'];
+$location = $_POST['location'];
+$querySuccess = false;
+$querySuccess2 = false;
+$querySuccess3 = false;
+
+
+// echo('cid '.$customerID.'amount '.$amount.'pickup '.$pickup.'location '.$location." end ");
+
+
 require 'config.php';
-$customerID = $_GET['customerID'];
-$amount = $_GET['amount'];
-//Get CSAs associated with Customer ID
-$query = $dbo->query("SELECT * FROM Cust_has_CSA WHERE cid='$customerID'");
-$array = $query->fetchAll();
-print_r($array.'<br />');
-$arrayOne = $array[0];
-$arrayTwo = $array[1];
-print_r($arrayOne.'<br />');
-print_r($arrayTwo.'<br />');
-$csaOne = $arrayOne[1];
-$locationOne = $arrayOne[2];
-echo ($csaOne.'<br />');
-echo ($locationOne.'<br />');
-$csaTwo = $arrayTwo[1];
-$locationTwo = $arrayTwo[2];
-echo ($csaTwo.'<br />');
-echo ($locationTwo.'<br />');
 
-//Check if csaOne is marketshare, csa, or coffee
-if($csaOne == 1 || $csaOne == 2 || $csaOne == 5 || $csaOne == 6 || $csaOne == 7 || $csaOne == 8) {
-	//CSA or coffee, add to Cust_makes_Pickup
-	$query = $dbo->prepare("INSERT INTO Cust_makes_Pickup (cid, lid) values ('$customerID','$locationOne')");
-	$query->execute(array('cid'=>$customerID, 'lid'=>$locationOne));
-}
-if($csaOne == 3 || $csaOne == 4) {
-	//Marketshare, update balance and add to transaction list
-	$query = $dbo->query("SELECT balance FROM Cust_has_bal WHERE cid='$customerID'");
-	print_r($query->errorInfo());
-	$array = $query->fetchAll();
-	$balance = $array[1];
-	$balance = $balance - $amount;
-	$query = $dbo->query("UPDATE Cust_has_Bal SET balance='$balance' WHERE cid='$customerID'");
-	print_r($query->errorInfo());
-	$query = $dbo->prepare("INSERT INTO Transaction (cid, tPrice, lid) values ('$customerID', '$amount', '$locationOne')");
-	$query->execute(array('cid'=>$customerID, 'tPrice'=>$amount, 'lid'=>$locationOne));
-	print_r($query->errorInfo());
-}
-//Check if csaTwo is marketshare, csa, or coffee
-if($csaTwo == 1 || $csaTwo == 2 || $csaTwo == 5 || $csaTwo == 6 || $csaTwo == 7 || $csaTwo == 8) {
-	//CSA or coffee, add to Cust_makes_Pickup
-	$query = $dbo->prepare("INSERT INTO Cust_makes_Pickup (cid, lid) values ('$customerID','$locationTwo')");
-	$query->execute(array('cid'=>$customerID, 'lid'=>$locationTwo));
-}
-if($csaTwo == 3 || $csaTwo == 4) {
-	//Marketshare, update balance and add to transaction list
-	$query = $dbo->query("SELECT balance FROM Cust_has_bal WHERE cid='$customerID'");
-	print_r($query->errorInfo());
-	$array = $query->fetchAll();
-	$balance = $array[1];
-	$balance = $balance - $amount;
-	$query = $dbo->query("UPDATE Cust_has_Bal SET balance='$balance' WHERE cid='$customerID'");
-	print_r($query->errorInfo());
-	$query = $dbo->prepare("INSERT INTO Transaction (cid, tPrice, lid) values ('$customerID', '$amount', '$locationTwo')");
-	$query->execute(array('cid'=>$customerID, 'tPrice'=>$amount, 'lid'=>$locationTwo));
-	print_r($query->errorInfo());
+//If Marketshare
+if(!empty($amount)) {
+	//Update Balance
+	$query = $dbo->prepare("UPDATE Cust_has_Bal SET balance=? WHERE cid=?");
+		if($query->execute(array($amount,$customerID))) {
+			$querySuccess = true;
+		} else {
+			echo ("<p>1 - Error inserting Customer has balance into database.  Please contact Site Administrator.</p>");
+		}
+	//Record Transaction
+	// echo("amount not empty");
+	$query2 = $dbo->prepare("INSERT INTO Transaction (cid, tPrice, lid) values ('$customerID', '$amount', '$location')");
+		if($query2->execute(array('cid'=>$customerID, 'tPrice'=>$amount, 'lid'=>$location))) {
+			$querySuccess2 = true;
+		} else {
+			echo ("<p>1 - Error inserting Transaction into database.  Please contact Site Administrator.</p>");
+		}
 }
 
-$result = $dbo->prepare("SELECT * FROM Cust_makes_Pickup");
-$result->execute();
-echo "<table>";
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	echo "<tr>";
-	foreach($row as $value) {
-		echo "<td>{$value}</td>";
-	}
-	echo "</tr>";
+//If CSA
+if($pickup != 0) {
+	// echo("pickup not zero");
+	$query = $dbo->prepare("INSERT INTO Cust_makes_Pickup (cid, lid) values ('$customerID','$location')");
+		if($query->execute(array('cid'=>$customerID, 'lid'=>$location))) {
+			$querySuccess3 = true;
+		} else {
+			echo ("<p>1 - Error inserting Customer makes pick up into database.  Please contact Site Administrator.</p>");
+		}
 }
-echo "</table>";
-echo "<br />";
-$result = $dbo->prepare("SELECT * FROM Cust_has_Bal");
-$result->execute();
-echo "<table>";
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	echo "<tr>";
-	foreach($row as $value) {
-		echo "<td>{$value}</td>";
-	}
-	echo "</tr>";
+
+if($querySuccess) {
+	echo ("<p>Customer has balance successfully entered into database.</p>");
+	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
 }
-echo "</table>";
-echo "<br />";
-$result = $dbo->prepare("SELECT * FROM Transaction");
-$result->execute();
-echo "<table>";
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-	echo "<tr>";
-	foreach($row as $value) {
-		echo "<td>{$value}</td>";
-	}
-	echo "</tr>";
+if($querySuccess2) {
+	echo ("<p>Transaction successfully entered into database.</p>");
+	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
 }
-echo "</table>";
-echo "<br />";
+if($querySuccess3) {
+	echo ("<p>Customer makes pickup successfully entered into database.</p>");
+	// echo ("<p>Redirecting to <a href='../main.php'>homepage</a></p>");
+}
+
+echo("<p><a href='../admin/admin.php'>Return to the Dashboard</a></p>");
+
 ?>
